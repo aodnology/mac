5dcf571ba5e445f069f6f296a50ef7c600de79ee
trap909 <aodnology@gmail.com>
2022-03-23 01:54:27 +0900

    for 기본 틀

---
+++ src/main/java/for_basic.java
@@ -0,0 +1,11 @@
+public class for_basic {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<10; i++){
+            System.out.println("coding"+i);
+        }
+    }
+}
+/* for (초기화; 종료조건; 반복실행) {
+   반복적으로 실행할 구문
+*/
