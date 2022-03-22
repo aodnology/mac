84344c2095c986c958aaec94c2302634abccc750
trap909 <aodnology@gmail.com>
2022-03-23 01:52:50 +0900

    별찍기

---
+++ src/main/java/for_2.java
@@ -0,0 +1,10 @@
+public class for_2 {
+    public static void main(String[] args){
+        for(int a = 0; a<5; a++){
+            for(int b = 0; b<a; b++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
