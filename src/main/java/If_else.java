db44c97662d28bc85bbea28280b2d382a71d3211
trap909 <aodnology@gmail.com>
2022-03-28 02:28:54 +0900

    if else 기본 개념

---
+++ src/main/java/If_else.java
@@ -0,0 +1,9 @@
+public class If_else {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println(1); // if 값이 true 면 then 절이 출력
+        } else {
+            System.out.println(2); // if 값이 false면 else 중괄호 안의 값이 출력
+        }
+    }
+}
