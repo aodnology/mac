256afa9ca8b3f0395c93ef3fc405e96a97bac517
trap909 <aodnology@gmail.com>
2022-03-28 01:13:59 +0900

    비교 연산자

---
+++ src/main/java/Equal_.java
@@ -0,0 +1,11 @@
+public class Equal_ {
+    public static void main(String[] args) {
+        System.out.println(1 == 2);
+        System.out.println(1 == 1);
+        System.out.println("one" == "two");
+        System.out.println("one" == "one");
+        // 비교 연산자는 true 와 false 의 값을 도출
+        // != ( not 의 의미, 비교값이 다른 경우 true 비교 값이 같은 경우 false)
+        // <,> ( 크기의 비교 )
+    }
+}
