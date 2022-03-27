ebde15fc4ca492a19bdfce48c840daeee6414aea
trap909 <aodnology@gmail.com>
2022-03-28 03:05:01 +0900

    if 중첩 기본 개념

---
+++ src/main/java/If_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class If_3 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String a,b,c = sc.nextLine();
+
+        if(false){
+            if(true){
+                System.out.println(a+" "+"coding");
+            } else {
+                System.out.println(b+" "+"coding");
+            }
+        }else{
+            System.out.println(c+" "+"coding");
+        }
+    }
+}
