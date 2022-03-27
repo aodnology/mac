6207ef37e831b509c8eae715e58a1dd77209d00b
trap909 <aodnology@gmail.com>
2022-03-28 00:58:25 +0900

    단항 연산자 ++ 개념

---
+++ src/main/java/cast_3.java
@@ -0,0 +1,12 @@
+public class cast_3 {
+    public static void main(String[] args) {
+        int i =3;
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        System.out.println(++i); // 6
+        System.out.println(i++); // 6
+        System.out.println(++i); // 8
+    }
+}
