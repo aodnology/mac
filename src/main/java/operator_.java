b32ab795d6bb7ec9bedafac29512e741ff300312
trap909 <aodnology@gmail.com>
2022-03-28 00:40:18 +0900

    연산자 나머지

---
+++ src/main/java/operator_.java
@@ -0,0 +1,13 @@
+public class operator_ {
+    public static void main(String[] args) {
+        int a = 3;
+        System.out.println(0 % a);
+        System.out.println(1 % a);
+        System.out.println(2 % a);
+        System.out.println(3 % a);
+        System.out.println(4 % a);
+        System.out.println(5 % a);
+        System.out.println(6 % a);
+        //나머지
+    }
+}
