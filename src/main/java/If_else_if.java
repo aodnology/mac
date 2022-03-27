a61ea94f1a8569cbf8bf946d7c058d384ed42dc6
trap909 <aodnology@gmail.com>
2022-03-28 02:42:00 +0900

    if else if else 기본 개념

---
+++ src/main/java/If_else_if.java
@@ -0,0 +1,13 @@
+public class If_else_if {
+    public static void main(String[] args) {
+        if (false) {                 // 조건문에 값을 대입했을때 경우에 따라 true, false 로 나누어져야 조건문으로써의 효용이 있다
+            System.out.println(1);
+        } else if (false) {
+            System.out.println(2);
+        } else if (false) {          // else if 는 생략,중복 사용 가능
+            System.out.println(3);
+        } else {                     // 생략 가능
+            System.out.println(4);
+        }                            // 조건문 실행시 true 를 만족할때 값이 출력된다.
+    }
+}
