c6ddc48c5b819830a44a3075e1088c6e77340688
trap909 <aodnology@gmail.com>
2022-03-29 22:19:36 +0900

    2022.3.29 문자열 길이 구하기

---
+++ src/main/java/Length_.java
@@ -0,0 +1,25 @@
+public class Length_ {
+    public static void main(String[] args) {
+        //length = 길이를 알고자 할 때 사용한다.
+
+        String car1 = "ford";
+        String car2 = "chevy";
+        String car3 = "mitsubishi";
+        String str = " I really love cars a great deal. no";
+
+        System.out.println(car1);
+        System.out.println(car1.length());
+
+        System.out.println();
+        System.out.println(car2);
+        System.out.println(car2.length());
+
+        System.out.println();
+        System.out.println(car3);
+        System.out.println(car3.length());
+
+        System.out.println();
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
