ef5ccd9f78af8ba371719d143f73a6d893d27e9b
trap909 <aodnology@gmail.com>
2022-03-30 22:34:04 +0900

    2022.3.30 array 기본 개념

---
+++ src/main/java/Array_.java
@@ -0,0 +1,11 @@
+public class Array_ {
+    public static void main(String[] args) {
+        // array : 연관된 데이터를 모아서 관리하기 위해 사용하는 데이터 타입
+
+        String[] classGroup = {"최진혁", "최유빈", "한이람", "이고잉"}; // 배열의 숫자는 0부터 시작되고 /최진혁(element,원소), 0(index,색인)/
+        System.out.println(classGroup[0]);
+        System.out.println(classGroup[1]);
+        System.out.println(classGroup[2]);
+        System.out.println(classGroup[3]);
+    }                             // index(색인) 0,1,2,3
+}
