85d3b48b489319a0066c943be83e26eb816d676b
trap909 <aodnology@gmail.com>
2022-04-06 00:15:07 +0900

    2022.4.6 백준 if 주사위

--- src/main/java/If_else_if.java
+++ src/main/java/If_else_if.java
@@ -1,10 +1,10 @@
 public class If_else_if {
     public static void main(String[] args) {
-        if (false) {                 // 조건문에 값을 대입했을때 경우에 따라 true, false 로 나누어져야 조건문으로써의 효용이 있다
+        if (true) {                 // 조건문에 값을 대입했을때 경우에 따라 true, false 로 나누어져야 조건문으로써의 효용이 있다
             System.out.println(1);
         } else if (false) {
             System.out.println(2);
-        } else if (false) {          // else if 는 생략,중복 사용 가능
+        } else if (true) {          // else if 는 생략,중복 사용 가능
             System.out.println(3);
         } else {                     // 생략 가능
             System.out.println(4);
