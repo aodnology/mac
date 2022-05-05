6149ef3f313f8024ac271291f219a3a1125c8a9f
trap909 <aodnology@gmail.com>
2022-05-05 20:49:01 +0900

    2022.05.05 메소드 개념

---
+++ src/main/java/method_.java
@@ -0,0 +1,20 @@
+public class method_ {
+    public static void numbering() {
+        //numbering 이라는 메소드를 정의
+        int i = 0;
+        while (i < 10) {
+        System.out.println(i);
+        i++;
+    }
+}           //정의
+
+    public static void main(String[] args) {
+        numbering(); //호출
+        /*
+        int i = 0;
+        while (i < 10) {
+        System.out.println(i);
+        i++; 과 같은 의미를 갖는다.
+         */
+    }
+}
