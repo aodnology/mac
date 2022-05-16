dd8d4a1e1f0aa9cf9282e2ba3ac74df9bbd7ac02
trap909 <aodnology@gmail.com>
2022-05-16 19:08:06 +0900

    2022.05.16 HashMap 개념 2

---
+++ src/main/java/Hashmap_2.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hashmap_2 {
+    public static void main(String[] args) {
+
+        List<Map<String, String>> list = new ArrayList<>();
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "박수호");
+        map.put("email", "aodnology@gmail.com");
+        map.put("addr", "서울");
+        map.put("dept", "데이터분석과");
+
+        list.add(map);
+
+        map = null;
+
+        map = new HashMap<>();
+
+        map.put("name", "홍길동");
+        map.put("email", "g.dhong@kopo.ac.kr");
+        map.put("addr", "경기");
+        map.put("dept", "디지털콘텐츠과");
+
+        list.add(map);
+
+        map = null;
+
+        int listSize = list.size();
+
+        for (int i = 0; i < listSize; i++) {
+
+            Map<String, String> rmap = list.get(i);
+            System.out.println("name : "+ rmap.get("name"));
+            System.out.println("email : "+ rmap.get("email"));
+            System.out.println("addr : "+ rmap.get("addr"));
+            System.out.println("dept : "+ rmap.get("dept"));
+            System.out.println("-----------------------------");
+        }
+
+    }
+}
+
