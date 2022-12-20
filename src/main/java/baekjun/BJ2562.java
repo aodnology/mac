6fe6638ec4e2784e408a5108ba2736ca02cef38a
trap909 <aodnology@gmail.com>
2022-12-20 23:48:36 +0900

    2022.12.20

---
+++ src/main/java/baekjun/BJ2562.java
@@ -0,0 +1,23 @@
+package baekjun;
+
+import java.util.Scanner;
+
+public class BJ2562 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int arr[] = new int[9];
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = s.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index + 1); // 0부터 시작
+    }
+}
